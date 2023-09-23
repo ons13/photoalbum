9287ee6cb911723a5bf9d51672cdcd54dfdfae93
Ons <63161976+ons13@users.noreply.github.com>
2023-09-23 11:35:50 +0100

    updating templ/models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Photo(models.Model):
+        Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+        description = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
