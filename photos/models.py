01790b7259b28bd81af3b5e0598b33b8d5ae7f13
Ons <63161976+ons13@users.noreply.github.com>
2023-10-11 17:10:17 +0100

    yala

--- photos/models.py
+++ photos/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-
     def __str__(self):
         return self.name
     
-    class Photo(models.Model):
-        Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-        description = models.CharField(max_length=500, null=False, blank=False)
+class Photo(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
 
     def __str__(self):
         return self.name
