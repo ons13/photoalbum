eafd387923259d36e560e06b453fe44bef6bdd55
--global <--global>
2024-01-07 14:47:26 +0100

    dep2

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2b!)-s%=e9cy7*)v-kg&&u8t%kp1p28)&hq0&vcjygml6_eoc&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
